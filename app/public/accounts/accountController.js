4c49f1117d548671466e3aafc1793b0a6aa63160
tonio5613 <ftpokerdeal@gmail.com>
2016-02-10 15:58:45 +0100

    ng-disabled ds account

--- app/public/accounts/accountController.js
+++ app/public/accounts/accountController.js
@@ -1,26 +1,50 @@
 'use strict';
 
 angular.module('timeShareApp')
-  .controller('accountController', function($scope, $http) {
+  .controller('accountController', function($scope, $http, authentication) {
 
     console.log('Hello from Account controller');
+    //console.log(authentication.currentUser().mail);
+
+   // var ac = this;
+
+    //ac.currentUser = authentication.currentUser();
+
 
     var refresh = function() {
+
+      if (authentication.isLoggedIn())
+      {
       $http.get('/api/account').success(function(response) {
 
         $scope.accountlist = response;
         $scope.account = '';
       });
+      }
+      else
+      {
+        $scope.accountlist = '';
+        $scope.account = '';
+        $scope.unclick = 'unclick';
+        //console.log('Unclick ?: ' + $scope.unclick);
+
+      }
     };
 
     refresh();
 
     $scope.addAccount = function() {
+          if (authentication.isLoggedIn())
+      {
       console.log($scope.account);
       $http.post('/api/account', $scope.account).success(function(response) {
         console.log(response);
         refresh();
       });
+    }
+      else
+      {
+      }
     };
 
     $scope.removeAccount = function(id) {
