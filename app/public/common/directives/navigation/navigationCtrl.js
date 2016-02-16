116011250b0e610277cfe37b7c10fb58209f0050
carogit <carowebdev@gmail.com>
2016-02-16 09:07:48 +0100

    debut css

--- app/public/common/directives/navigation/navigationCtrl.js
+++ app/public/common/directives/navigation/navigationCtrl.js
@@ -2,11 +2,14 @@
 
   angular
     .module('timeShareApp')
+    .run(['$anchorScroll', function($anchorScroll) {
+  $anchorScroll.yOffset = 50;   // always scroll by 50 extra pixels
+}])
     .controller('navigationCtrl', navigationCtrl);
 
-  navigationCtrl.$inject = ['$location', 'authentication'];
+  navigationCtrl.$inject = ['$anchorScroll','$location', 'authentication', '$scope'];
 
-  function navigationCtrl($location, authentication) {
+  function navigationCtrl($anchorScroll,$location, authentication, $scope) {
     var vm = this;
 
     vm.currentPath = $location.path();
@@ -19,6 +22,18 @@
       authentication.logout();
       $location.path('/');
     };
-
+    $scope.gotoAnchor = function(x) {
+      var newHash = 'anchor' + x;
+      if ($location.hash() !== newHash) {
+        // set the $location.hash to `newHash` and
+        // $anchorScroll will automatically scroll to it
+        $location.hash('anchor' + x);
+      } else {
+        // call $anchorScroll() explicitly,
+        // since $location.hash hasn't changed
+        $anchorScroll();
+      }
+    };
   }
+  
 })();
