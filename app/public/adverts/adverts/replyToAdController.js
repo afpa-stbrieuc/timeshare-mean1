b8487e24ad93d8847f678068adfeb6acd88710dd
tonio5613 <ftpokerdeal@gmail.com>
2016-02-22 17:00:13 +0100

    integration date picker bootstrap

--- app/public/adverts/adverts/replyToAdController.js
+++ app/public/adverts/adverts/replyToAdController.js
@@ -16,6 +16,7 @@ angular.module('timeShareApp')
 $scope.returnPage = $location.search().page || '/';
 
     $scope.addReply = function () {
+        $scope.reply.work_date = $scope.work_date;
         $scope.reply.author = authentication.currentUser().firstname;
         $scope.reply.author_id = authentication.currentUser()._id;
         $scope.reply.toAdId = $routeParams._id;
@@ -62,4 +63,85 @@ $scope.returnPage = $location.search().page || '/';
         $scope.reply = "";
     };
 
+     $scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+
+  $scope.clear = function() {
+    $scope.dt = null;
+  };
+
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+    return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+  };
+
+  $scope.toggleMin = function() {
+    $scope.minDate = $scope.minDate ? null : new Date();
+  };
+
+  $scope.toggleMin();
+  $scope.maxDate = new Date(2020, 5, 22);
+
+  $scope.open1 = function() {
+    $scope.popup1.opened = true;
+  };
+
+  $scope.open2 = function() {
+    $scope.popup2.opened = true;
+  };
+
+  $scope.setDate = function(year, month, day) {
+    $scope.dt = new Date(year, month, day);
+  };
+
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1
+  };
+
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
+  $scope.altInputFormats = ['M!/d!/yyyy'];
+
+  $scope.popup1 = {
+    opened: false
+  };
+
+  $scope.popup2 = {
+    opened: false
+  };
+
+  var tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  var afterTomorrow = new Date();
+  afterTomorrow.setDate(tomorrow.getDate() + 1);
+  $scope.events =
+    [
+      {
+        date: tomorrow,
+        status: 'full'
+      },
+      {
+        date: afterTomorrow,
+        status: 'partially'
+      }
+    ];
+
+  $scope.getDayClass = function(date, mode) {
+    if (mode === 'day') {
+      var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+      for (var i = 0; i < $scope.events.length; i++) {
+        var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+        if (dayToCheck === currentDay) {
+          return $scope.events[i].status;
+        }
+      }
+    }
+
+    return '';
+  }
 }]);
